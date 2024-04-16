2dac2415ff74ec9fccd6c4a69f4fef9a2cb1d68d
Claudinei de Lima <cfrdlxava@gmail.com>
2024-04-15 21:41:30 -0300

    ajustes e adicao na animacao da arrow

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,46 @@ window.addEventListener("scroll", function () {
   if (this.window.scrollY > 0) return navbar.classList.add("active");
   return navbar.classList.remove("active");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var arrowImage = document.getElementById("rotating-arrow-web");
+  var infoDiv = document.getElementById("info-div-web");
+  arrowImage.addEventListener("click", function () {
+    arrowImage.classList.toggle("rotate180-web");
+    arrowImage.style.transition = "transform 0.5s ease"; // Adiciona a transição suave
+    // Mostra ou esconde a div de informações dependendo do estado atual
+    if (infoDiv.style.display === "none") {
+      infoDiv.style.display = "block";
+    } else {
+      infoDiv.style.display = "none";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var arrowImage = document.getElementById("rotating-arrow-mobile");
+  var infoDiv = document.getElementById("info-div-mobile");
+  arrowImage.addEventListener("click", function () {
+    arrowImage.classList.toggle("rotate180-mobile");
+    arrowImage.style.transition = "transform 0.5s ease"; // Adiciona a transição suave
+    if (infoDiv.style.display === "none") {
+      infoDiv.style.display = "block";
+    } else {
+      infoDiv.style.display = "none";
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var arrowImage = document.getElementById("rotating-arrow-games");
+  var infoDiv = document.getElementById("info-div-games");
+  arrowImage.addEventListener("click", function () {
+    arrowImage.classList.toggle("rotate180-games");
+    arrowImage.style.transition = "transform 0.5s ease"; // Adiciona a transição suave
+    if (infoDiv.style.display === "none") {
+      infoDiv.style.display = "block";
+    } else {
+      infoDiv.style.display = "none";
+    }
+  });
+});
