d6aaf05d04d219eb2627c825928bc6231f73fddf
Claudinei de Lima <cfrdlxava@gmail.com>
2024-04-15 23:56:50 -0300

    Finalizacao da animacao nas skills e otimizacao no js

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,61 @@
-const navbar = document.querySelector(".navbar");
-const mobileNavbar = document.querySelector(".navbar__mobile");
-const button = document.querySelector(".burguer");
-
-button.addEventListener("click", function () {
-  mobileNavbar.classList.toggle("active");
-});
-
-window.addEventListener("scroll", function () {
-  if (this.window.scrollY > 0) return navbar.classList.add("active");
-  return navbar.classList.remove("active");
-});
-
 document.addEventListener("DOMContentLoaded", function () {
-  var arrowImage = document.getElementById("rotating-arrow-web");
-  var infoDiv = document.getElementById("info-div-web");
-  arrowImage.addEventListener("click", function () {
-    arrowImage.classList.toggle("rotate180-web");
-    arrowImage.style.transition = "transform 0.5s ease"; // Adiciona a transição suave
-    // Mostra ou esconde a div de informações dependendo do estado atual
-    if (infoDiv.style.display === "none") {
-      infoDiv.style.display = "block";
-    } else {
-      infoDiv.style.display = "none";
-    }
+  // Função para lidar com a rotação da seta
+  function toggleArrowRotation(arrowImage) {
+    arrowImage.classList.toggle("rotate180-web"); // Use a classe correta aqui
+  }
+
+  // Função para lidar com a exibição/ocultação da div de informações
+  function toggleInfoDiv(infoDiv, displayValue) {
+    infoDiv.style.display = displayValue;
+  }
+
+  function addEvents(divGeral, arrowImage, infoDiv) {
+    // Adiciona eventos para a rotação da seta quando o mouse passa sobre divGeral
+    divGeral.addEventListener("mouseenter", function () {
+      toggleArrowRotation(arrowImage);
+    });
+
+    divGeral.addEventListener("mouseleave", function () {
+      toggleArrowRotation(arrowImage);
+    });
+
+    // Adiciona evento para a exibição da div de informações quando o mouse passa sobre divGeral
+    divGeral.addEventListener("mouseenter", function () {
+      toggleInfoDiv(infoDiv, "block");
+    });
+
+    // Adiciona evento para a ocultação da div de informações quando o mouse sai de divGeral
+    divGeral.addEventListener("mouseleave", function () {
+      toggleInfoDiv(infoDiv, "none");
+    });
+  }
+
+  const divGeralWeb = document.querySelector(".skills__item-web");
+  const arrowImageWeb = document.getElementById("rotating-arrow-web");
+  const infoDivWeb = document.getElementById("info-div-web");
+  addEvents(divGeralWeb, arrowImageWeb, infoDivWeb);
+
+  const divGeralMobile = document.querySelector(".skills__item-mobile");
+  const arrowImageMobile = document.getElementById("rotating-arrow-mobile");
+  const infoDivMobile = document.getElementById("info-div-mobile");
+  addEvents(divGeralMobile, arrowImageMobile, infoDivMobile);
+
+  const divGeralGames = document.querySelector(".skills__item-games");
+  const arrowImageGames = document.getElementById("rotating-arrow-games");
+  const infoDivGames = document.getElementById("info-div-games");
+  addEvents(divGeralGames, arrowImageGames, infoDivGames);
+
+  // Adiciona eventos para a barra de navegação e o botão de menu
+  const navbar = document.querySelector(".navbar");
+  const mobileNavbar = document.querySelector(".navbar__mobile");
+  const button = document.querySelector(".burguer");
+
+  button.addEventListener("click", function () {
+    mobileNavbar.classList.toggle("active");
   });
-});
 
-document.addEventListener("DOMContentLoaded", function () {
-  var arrowImage = document.getElementById("rotating-arrow-mobile");
-  var infoDiv = document.getElementById("info-div-mobile");
-  arrowImage.addEventListener("click", function () {
-    arrowImage.classList.toggle("rotate180-mobile");
-    arrowImage.style.transition = "transform 0.5s ease"; // Adiciona a transição suave
-    if (infoDiv.style.display === "none") {
-      infoDiv.style.display = "block";
-    } else {
-      infoDiv.style.display = "none";
-    }
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  var arrowImage = document.getElementById("rotating-arrow-games");
-  var infoDiv = document.getElementById("info-div-games");
-  arrowImage.addEventListener("click", function () {
-    arrowImage.classList.toggle("rotate180-games");
-    arrowImage.style.transition = "transform 0.5s ease"; // Adiciona a transição suave
-    if (infoDiv.style.display === "none") {
-      infoDiv.style.display = "block";
-    } else {
-      infoDiv.style.display = "none";
-    }
+  window.addEventListener("scroll", function () {
+    if (this.window.scrollY > 0) return navbar.classList.add("active");
+    return navbar.classList.remove("active");
   });
 });
