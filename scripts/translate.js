99837e1d6708cb8fcb914071c0f550df5c9719e9
Claudinei de Lima <cfrdlxava@gmail.com>
2024-05-06 21:54:05 -0300

    translate funcionando, falta descobrir porque nao esta pegando todo body

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,41 +1,31 @@
-// const toggle = document.getElementById("toggle");
-
-// function trocarIdioma(idioma) {
-//   fetch(idioma + ".json")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       document.getElementById("inicio").innerText = data.inicio;
-//       document.getElementById("sobre").innerText = data.sobre;
-//       document.getElementById("skills").innerText = data.skills;
-//       document.getElementById("projetos").innerText = data.projetos;
-//       document.getElementById("contato").innerText = data.contato;
-//     })
-//     .catch((error) => console.error("Erro ao carregar traduções:", error));
-// }
-
 function consumirAPIGoogleTranslate() {
-  var htmlOriginal = document.body.innerHTML;
+  var htmlOriginal = document.innerHTML;
+  var idiomaOrigem = "pt";
+  var idiomaDestino = "en";
+  var formato = "html";
 
-  // Use a API de Tradução do Google para traduzir o conteúdo HTML
   const settings = {
     async: true,
     crossDomain: true,
-    url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
+    url: "https://deep-translate1.p.rapidapi.com/language/translate/v2",
     method: "POST",
     headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      "X-RapidAPI-Key": "e5a2399fb2msh174450145c0b032p109d5fjsnc6364705946c",
-      "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+      "content-type": "application/json",
+      "X-RapidAPI-Key": "a9d943913bmsh4f69fdea2f8f4aap1fa5e1jsnb8c5d72de41f",
+      "X-RapidAPI-Host": "deep-translate1.p.rapidapi.com",
     },
-    data: {
-      q: htmlOriginal, // Traduz todo o conteúdo HTML
-      target: "en", // Idioma de destino (exemplo: inglês)
-      source: "pt", // Idioma de origem (exemplo: português)
-    },
+    processData: false,
+    data: JSON.stringify({
+      q: htmlOriginal,
+      source: idiomaOrigem,
+      target: idiomaDestino,
+      format: formato,
+    }),
   };
 
   $.ajax(settings).done(function (response) {
-    // Atualize o conteúdo HTML traduzido na sua página
-    document.body.innerHTML = response.data.translations[0].translatedText;
+    console.log(response);
+    var translatedText = response.data.translations.translatedText;
+    document.body.innerHTML = translatedText;
   });
 }
